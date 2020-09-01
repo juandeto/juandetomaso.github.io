4bf7d7fae68235f4524c4179b5c7c1eb12513c94
juandeto <juandeto10@gmail.com>
2020-09-01 15:42:30 -0300

    new links

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 
 const hamurgerBtn=document.querySelector('.hamburger');
 const menu=document.querySelector('.menu-list');
+const menuBtn=document.querySelectorAll('.menu-list li');
 
 const toggleClass=()=>{
     menu.classList.toggle('show');
     hamurgerBtn.classList.toggle('rotate')
 }
 
+const closeMenu=()=>{
+    if(menu.classList.contains('show')){
+        menu.classList.remove('show');
+        hamurgerBtn.classList.toggle('rotate')
+    }
+}
+
 hamurgerBtn.addEventListener('click', toggleClass);
+menuBtn.forEach(btn => btn.addEventListener('click', closeMenu))
